bde6686865c77ef981981579e86fa5590e4f2bd3
trevca <trevorcarpenter4@gmail.com>
2021-02-24 17:28:44 -0800

    app part done

--- LinkedInScraper/LinkedInScraper/ViewController.swift
+++ LinkedInScraper/LinkedInScraper/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var URLTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goPressed(_ sender: Any) {
+        
+        guard let url = URLTextField.text else {
+            print("Text Field Error")
+            return
+        }
+        
+        
+    }
+    
 }
 
